4f82ad71ecf40fa58ec53d0e8b683642959d67e3
matsarat <mat.saratowicz@gmail.com>
2020-10-14 18:56:18 +0200

    Add method getting input for move from player

--- src/main/java/com/example/java/maven/game2048/UserInputProvider.java
+++ src/main/java/com/example/java/maven/game2048/UserInputProvider.java
@@ -3,14 +3,18 @@ package com.example.java.maven.game2048;
 import java.util.Scanner;
 
 public class UserInputProvider {
-    public static int getIntFromUser(){
+    public static String getIntFromUser(){
         while (true){
             try {
-                return new Scanner(System.in).nextInt();
+                return new Scanner(System.in).next();
             } catch (Exception e) {
                 System.err.println("Dej mie inta");
             }
         }
     }
-
+    public static String getMoveFromPlayer(){
+        Scanner scanner = new Scanner(System.in);
+        String move = scanner.nextLine();
+        return move;
+    }
 }
