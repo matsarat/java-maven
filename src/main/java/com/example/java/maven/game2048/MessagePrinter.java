f37316f6364f45dd20a5fc46215bf5c17cdb722e
matsarat <mat.saratowicz@gmail.com>
2020-10-15 22:04:48 +0200

    Add instructions for player

--- src/main/java/com/example/java/maven/game2048/MessagePrinter.java
+++ src/main/java/com/example/java/maven/game2048/MessagePrinter.java
@@ -1,4 +1,18 @@
 package com.example.java.maven.game2048;
 
 public class MessagePrinter {
+
+    private MessagePrinter() {
+    }
+
+    public static void printInstructionsBeforeGame(){
+        String instructions = ("INSTRUCTIONS:" + '\n' +
+                "To move left   - insert a" + '\n' +
+                "To move right  - insert d" + '\n' +
+                "To move up     - insert w" +'\n' +
+                "To move down   - insert s" + '\n'
+        );
+
+        System.out.println(instructions);
+    }
 }
