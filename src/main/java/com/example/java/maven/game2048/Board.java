c60acd293be80df8f7e5df77e208ccfc83e3df09
matsarat <mat.saratowicz@gmail.com>
2020-10-17 21:56:14 +0200

    Change sorting and moving methods, delete unused methods

--- src/main/java/com/example/java/maven/game2048/Board.java
+++ src/main/java/com/example/java/maven/game2048/Board.java
@@ -23,11 +23,119 @@ public class Board {
         }
     }
 
-     //tym sie nie przejmuj, tego raczej nie bedziesz umial uzyc
-    private Board(Board toCopy){
+
+    public Board(Board toCopy) {
         this.numberOfRows = toCopy.numberOfRows;
         this.numberOfColumns = toCopy.numberOfColumns;
         this.fields = getCopyOfFields(toCopy);
+    }
+
+    public void placeTwoInitialElementsOnBoard() {
+        for (int i = 0; i < 2; i++) {
+            placeNextElementOnBoard();
+        }
+    }
+
+    public void placeNextElementOnBoard() {
+        Field nextElementOnBoard = getRandomFieldWithZeroValue();
+        nextElementOnBoard.setValue(2);
+    }
+
+    public void sumInRowOrColumnLeftOrUp(Field[] rowOrColumn) {
+        for (int index = 0; index < rowOrColumn.length - 1; index++) {
+            int currentField = rowOrColumn[index].getValue();
+            int nextField = rowOrColumn[index + 1].getValue();
+            if (currentField == nextField) {
+                rowOrColumn[index].addToValue(currentField);
+                rowOrColumn[index + 1].setValue(0);
+
+            }
+        }
+    }
+
+    public void sumInRowOrColumnRightOrDown(Field[] rowOrColumn) {
+        for (int index = (rowOrColumn.length - 1); index > 0; index--) {
+            int currentField = rowOrColumn[index].getValue();
+            int nextField = rowOrColumn[index - 1].getValue();
+            if (currentField == nextField) {
+                rowOrColumn[index].addToValue(currentField);
+                rowOrColumn[index - 1].setValue(0);
+
+            }
+        }
+    }
+
+    public void moveLeft() {
+        for (Field[] row : this.fields) {
+            while (notSortedLeftOrUp(row)) {
+                leftOrUpSort(row);
+            }
+        }
+    }
+
+    public void moveRight() {
+        for (Field[] row : this.fields) {
+            while (notSortedRightOrDown(row)) {
+                rightOrDownSort(row);
+            }
+        }
+    }
+
+    public void moveUp() {
+        for (Field[] column : getColumns()) {
+            while (notSortedLeftOrUp(column)) {
+                leftOrUpSort(column);
+            }
+        }
+    }
+
+    public void moveDown() {
+        for (Field[] column : getColumns()) {
+            while (notSortedRightOrDown(column)) {
+                rightOrDownSort(column);
+            }
+        }
+    }
+
+    public void sumLeft() {
+        for (Field[] row : this.fields) {
+            sumInRowOrColumnLeftOrUp(row);
+        }
+    }
+
+    public void sumDown() {
+        for (Field[] column : getColumns()) {
+            sumInRowOrColumnRightOrDown(column);
+        }
+    }
+
+    public void sumUp() {
+        for (Field[] column : getColumns()) {
+            sumInRowOrColumnLeftOrUp(column);
+        }
+    }
+
+    public void sumRight() {
+        for (Field[] row : this.fields) {
+            sumInRowOrColumnRightOrDown(row);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(numberOfRows, numberOfColumns);
+        result = 31 * result + Arrays.hashCode(fields);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return numberOfRows == board.numberOfRows &&
+                numberOfColumns == board.numberOfColumns &&
+                Arrays.deepEquals(fields, board.fields);
     }
 
     @Override
@@ -45,74 +153,51 @@ public class Board {
         return strBoardBuilder.toString().trim();
     }
 
-    public void placeTwoInitialElementsOnBoard() {
-        for (int i = 0; i < 2; i++) {
-            placeNextElementOnBoard();
-        }
-    }
-
-    public void placeNextElementOnBoard() {
-        Field nextElementOnBoard = getRandomFieldWithZeroValue();
-        nextElementOnBoard.setValue(2);
-    }
-
-    public void sumInRowOrColumn(Field[] rowOrColumn) {
-        for (int index = 0; index < rowOrColumn.length - 1; index++) {
-            Field currentField = rowOrColumn[index];
-            Field nextField = rowOrColumn[index + 1];
-            if (currentField.getValue() == nextField.getValue()) {
-                currentField.addToValue(nextField.getValue());
-                nextField.setValue(0);
-            }
-        }
-    }
-
-    public void moveLeft() {
+    public boolean checkIfWon(int winningNumber) {
         for (Field[] row : this.fields) {
-            while (sortedLeftOrUp(row)) {
-                leftOrUpSort(row);
+            for (Field field : row) {
+                if (field.getValue() == winningNumber) {
+                    System.out.print(this);
+                    System.out.println("YOU WON!");
+                    return true;
+
+                }
             }
         }
+        return false;
     }
 
-    public void moveRight() {
+    public boolean checkIfSimilarNeighboringFields() {
         for (Field[] row : this.fields) {
-            while (sortedRightOrDown(row)) {
-                rightOrDownSort(row);
+            for (int index = 0; index < row.length - 1; index++) {
+                if (row[index].getValue() == row[index + 1].getValue()) {
+                    return true;
+                }
             }
         }
-    }
-
-    public void moveUp() {
         for (Field[] column : getColumns()) {
-            while (sortedRightOrDown(column)) {
-                rightOrDownSort(column);
+            for (int index = 0; index < column.length - 1; index++) {
+                if (column[index].getValue() == column[index + 1].getValue()) {
+                    return true;
+                }
             }
         }
+        return false;
     }
 
-    public void moveDown() {
-        for (Field[] column : getColumns()) {
-            while (sortedRightOrDown(column)) {
-                rightOrDownSort(column);
-            }
+    public boolean checkIfGameOver() {
+        int zeroValuesOnBoard = getFieldsWithZeroValue().size();
+        if (zeroValuesOnBoard > 0) {
+            return false;
         }
-    }
-
-    public void sumLeftOrRight() {
-        for (Field[] row : this.fields) {
-            sumInRowOrColumn(row);
+        if (checkIfSimilarNeighboringFields()) {
+            return false;
         }
+        System.out.println("YOU LOST!");
+        return true;
     }
 
-    public void sumUpOrDown() {
-        for (Field[] column : getColumns()) {
-            sumInRowOrColumn(column);
-        }
-    }
-
-
-    private Field[][] getCopyOfFields(Board toCopy){
+    private Field[][] getCopyOfFields(Board toCopy) {
         Field[][] copy = new Field[numberOfRows][numberOfColumns];
         for (int rowNumber = 0; rowNumber < numberOfColumns; rowNumber++) {
             for (int columnNumber = 0; columnNumber < numberOfRows; columnNumber++) {
@@ -120,48 +205,6 @@ public class Board {
             }
         }
         return copy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Board board = (Board) o;
-        return numberOfRows == board.numberOfRows &&
-                numberOfColumns == board.numberOfColumns &&
-                Arrays.deepEquals(fields, board.fields);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(numberOfRows, numberOfColumns);
-        result = 31 * result + Arrays.hashCode(fields);
-        return result;
-    }
-
-
-    public boolean canMoveUp() {
-        Board copyOfCurrentBoard = new Board(this);
-        copyOfCurrentBoard.moveUp();
-        return !copyOfCurrentBoard.equals(this);
-    }
-
-    public boolean canMoveDown() {
-        Board copyOfCurrentBoard = new Board(this);
-        copyOfCurrentBoard.moveDown();
-        return !copyOfCurrentBoard.equals(this);
-    }
-
-    public boolean canMoveRight() {
-        Board copyOfCurrentBoard = new Board(this);
-        copyOfCurrentBoard.moveRight();
-        return !copyOfCurrentBoard.equals(this);
-    }
-
-    public boolean canMoveLeft() {
-        Board copyOfCurrentBoard = new Board(this);
-        copyOfCurrentBoard.moveLeft();
-        return !copyOfCurrentBoard.equals(this);
     }
 
     private Field[] getColumn(int columnNumber) {
@@ -198,91 +241,47 @@ public class Board {
         return fieldsWithZeroValues.get(random.nextInt(fieldsWithZeroValues.size()));
     }
 
-    private boolean sortedLeftOrUp(Field[] rowOrColumn) {
+    private boolean notSortedLeftOrUp(Field[] rowOrColumn) {
         for (int index = 0; index < rowOrColumn.length - 1; index++) {
-            Field currentField = rowOrColumn[index];
-            Field nextField = rowOrColumn[index + 1];
-            if (currentField.getValue() == 0 && nextField.getValue() > 0) {
-                return false;
+            if (rowOrColumn[index].getValue() == 0 && rowOrColumn[index + 1].getValue() > 0) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
-    private boolean sortedRightOrDown(Field[] rowOrColumn) {
-        for (int index = rowOrColumn.length - 1; index > -1; index--) {
-            if (rowOrColumn[index].getValue() == 0 && rowOrColumn[index - 1].getValue() > 0) {
-                return false;
+    private boolean notSortedRightOrDown(Field[] rowOrColumn) {
+        for (int index = rowOrColumn.length - 1; index > 0; index--) {
+            int currentFieldValue = rowOrColumn[index].getValue();
+            int previousFieldValue = rowOrColumn[index - 1].getValue();
+            if (currentFieldValue == 0 && previousFieldValue > 0) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     private void leftOrUpSort(Field[] rowOrColumn) {
         for (int index = 0; index < rowOrColumn.length - 1; index++) {
-            Field currentField = rowOrColumn[index];
-            Field nextField = rowOrColumn[index + 1];
-            if (currentField.getValue() == 0 && nextField.getValue() > 0) {
-                int currentFieldValue = currentField.getValue();
-                int nextFieldValue = nextField.getValue();
-                currentField.setValue(nextFieldValue);
-                nextField.setValue(currentFieldValue);
+            int currentFieldValue = rowOrColumn[index].getValue();
+            int nextFieldValue = rowOrColumn[index + 1].getValue();
+            if (currentFieldValue == 0 && nextFieldValue > 0) {
+                rowOrColumn[index].setValue(nextFieldValue);
+                rowOrColumn[index + 1].setValue(currentFieldValue);
+
             }
         }
     }
 
     private void rightOrDownSort(Field[] rowOrColumn) {
-        for (int index = rowOrColumn.length - 1; index > -1; index--) {
-            if (rowOrColumn[index].getValue() == 0 && rowOrColumn[index - 1].getValue() > 0) {
-                int currentFieldValue = rowOrColumn[index].getValue();
-                int previousFieldValue = rowOrColumn[index - 1].getValue();
+        for (int index = rowOrColumn.length - 1; index > 0; index--) {
+            int currentFieldValue = rowOrColumn[index].getValue();
+            int previousFieldValue = rowOrColumn[index - 1].getValue();
+            if (currentFieldValue == 0 && previousFieldValue > 0) {
                 rowOrColumn[index].setValue(previousFieldValue);
                 rowOrColumn[index - 1].setValue(currentFieldValue);
             }
         }
-    }
-
-    public boolean checkIfWon() {
-        for (Field[] row : this.fields) {
-            for (Field field : row) {
-                if (field.getValue() == 2048) {
-                    System.out.println("YOU WON!");
-                    return true;
-
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkIfSimilarNeighboringFields() {
-        for (Field[] row : this.fields) {
-            for (int index = 0; index < row.length - 1; index++) {
-                if (row[index].getValue() == row[index+1].getValue()) {
-                    return true;
-                }
-            }
-        }
-        for (Field[] column : getColumns()) {
-            for (int index = 0; index < column.length - 1; index++) {
-                if (column[index].getValue() == column[index + 1].getValue()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkIfGameOver() {
-        int zeroValuesOnBoard = getFieldsWithZeroValue().size();
-        if (zeroValuesOnBoard > 0) {
-            return false;
-        }
-        if (checkIfSimilarNeighboringFields()) {
-            return false;
-        }
-        System.out.println("YOU LOST!");
-        return true;
     }
 
 }
