34a91e23cdc9c5c38e717520bcc23fd6b206ce6a
matsarat <mat.saratowicz@gmail.com>
2020-10-17 21:53:42 +0200

    Delete unused method

--- src/main/java/com/example/java/maven/game2048/UserInputProvider.java
+++ src/main/java/com/example/java/maven/game2048/UserInputProvider.java
@@ -3,16 +3,8 @@ package com.example.java.maven.game2048;
 import java.util.Scanner;
 
 public class UserInputProvider {
-    public static String getIntFromUser(){
-        while (true){
-            try {
-                return new Scanner(System.in).next();
-            } catch (Exception e) {
-                System.err.println("Dej mie inta");
-            }
-        }
-    }
-    public static String getMoveFromPlayer(){
+
+    public static String getMoveFromPlayer() {
         System.out.println("Next move: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
