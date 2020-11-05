997e3506dc4d1eb3bddefd61f7da38af31521215
matsarat <mat.saratowicz@gmail.com>
2020-11-05 22:15:39 +0100

    Initial commit to Black Jack game

---
+++ src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
@@ -0,0 +1,9 @@
+package com.example.java.maven.gameBlackJack;
+
+public class gameBlackJack {
+    public static void main(String[] args) {
+       Card card = new Card();
+       card.deckBuilder();
+
+    }
+}
