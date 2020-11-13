e8f4789ac1754faed8caf2125f8f761b4f54cb0c
matsarat <mat.saratowicz@gmail.com>
2020-11-13 21:14:36 +0100

    Change hand to ArrayList, extend constructor and add toString method

--- src/main/java/com/example/java/maven/gameBlackJack/Player.java
+++ src/main/java/com/example/java/maven/gameBlackJack/Player.java
@@ -1,21 +1,28 @@
 package com.example.java.maven.gameBlackJack;
 
-import java.util.Stack;
+import java.util.ArrayList;
 
 public class Player {
     String name;
-    Stack<Card> hand;
+    ArrayList<Card> hand;
     int points;
 
     public Player(String name) {
         this.name = name;
+        this.hand = new ArrayList<>();
+        this.points = 0;
     }
 
     public void addCardToPlayersHand(Card card) {
-        hand.push(card);
+        hand.add(card);
     }
 
     public int getPoints() {
         return points;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + hand;
     }
 }
