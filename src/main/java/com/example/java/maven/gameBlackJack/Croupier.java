511dd25859325ed0607e2ef1506226c8050497ce
matsarat <mat.saratowicz@gmail.com>
2020-11-13 22:15:37 +0100

    Add Croupier class

---
+++ src/main/java/com/example/java/maven/gameBlackJack/Croupier.java
@@ -0,0 +1,25 @@
+package com.example.java.maven.gameBlackJack;
+
+import java.util.ArrayList;
+
+public class Croupier {
+    String name;
+    ArrayList<Card> hand;
+    int points;
+
+    public Croupier() {
+        this.name = "Croupier";
+        this.hand = new ArrayList<>();
+        this.points = 0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + hand.get(0);
+    }
+
+}
