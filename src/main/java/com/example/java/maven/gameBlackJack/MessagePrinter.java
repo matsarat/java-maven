245d19cdee39acbf53a5fc67edbc64747f521358
matsarat <mat.saratowicz@gmail.com>
2020-11-13 21:35:05 +0100

    Add printHand method

--- src/main/java/com/example/java/maven/gameBlackJack/MessagePrinter.java
+++ src/main/java/com/example/java/maven/gameBlackJack/MessagePrinter.java
@@ -1,5 +1,7 @@
 package com.example.java.maven.gameBlackJack;
 
+import java.util.ArrayList;
+
 public class MessagePrinter {
 
     private MessagePrinter() {
@@ -12,4 +14,9 @@ public class MessagePrinter {
     public static void printError(String errorToPrint){
         System.err.println(errorToPrint);
     }
+
+    public static void printHand(ArrayList<Card> hand) {
+        System.out.println(hand);
+    }
+
 }
