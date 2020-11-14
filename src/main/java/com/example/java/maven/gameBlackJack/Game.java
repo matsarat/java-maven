a8a003eb60ace832672aaab6e39500fb832cb4e9
matsarat <mat.saratowicz@gmail.com>
2020-11-14 17:50:17 +0100

    Add getting and validating player's decision

--- src/main/java/com/example/java/maven/gameBlackJack/Game.java
+++ src/main/java/com/example/java/maven/gameBlackJack/Game.java
@@ -1,25 +1,24 @@
 package com.example.java.maven.gameBlackJack;
 
 public class Game {
+    Player player = new Player(UserInputProvider.getPlayersName());
+    Croupier croupier = new Croupier();
     private Deck deck;
     private final static String INSTRUCTIONS =
             ('\n' + "INSTRUCTIONS:" + '\n' +
-            "To take card   - insert H" + '\n' +
-            "To stand       - insert S" + '\n' +
-            '\n'
+            "To take card (hit)    - insert H" + '\n' +
+            "To do nothing (stand) - insert S"
             );
+    private final static String INVALID_INPUT = ("Invalid input! Please stick to instructions!");
 
 
     public Game() {
         this.deck = new Deck();
         initiateGame();
+        playersDecision();
     }
 
     public void initiateGame() {
-        Player player = new Player(UserInputProvider.getPlayersName());
-        Croupier croupier = new Croupier();
-
-
         player.hand.add(deck.getCard());
         croupier.hand.add(deck.getCard());
         player.hand.add(deck.getCard());
@@ -29,7 +28,31 @@ public class Game {
         MessagePrinter.printCroupier(croupier);
 
         MessagePrinter.printMessage(INSTRUCTIONS);
+    }
+
+    private boolean isPlayersDecisionValid(String userInput) {
+        if (userInput.equalsIgnoreCase("H")) {
+            return true;
+        }
+        else if (userInput.equalsIgnoreCase("S")) {
+            return true;
+        }
+        MessagePrinter.printError(INVALID_INPUT);
+        return false;
+    }
+
+    private String getPlayersDecision() {
+        String userInput = UserInputProvider.getPlayersDecision(player.name);
+        if (isPlayersDecisionValid(userInput)) {
+            return userInput;
+        }
+        return getPlayersDecision();
+    }
+
+    public void playersDecision() {
+        getPlayersDecision();
 
 
     }
+
 }
