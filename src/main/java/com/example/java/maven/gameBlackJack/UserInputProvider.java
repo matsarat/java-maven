6b42e500d827fb90ddb38149f20e32651a46be7d
matsarat <mat.saratowicz@gmail.com>
2020-11-14 17:51:34 +0100

    Add getPlayersDecision method and delete other unused method

--- src/main/java/com/example/java/maven/gameBlackJack/UserInputProvider.java
+++ src/main/java/com/example/java/maven/gameBlackJack/UserInputProvider.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 
 public class UserInputProvider {
 
-    public static String getMoveFromPlayer() {
-        MessagePrinter.printMessage("" + '\n' +
-                "Next move: ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
-
     public static String getPlayersName() {
         MessagePrinter.printMessage("" + '\n' +
                 "What is your name? ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
-
     }
+
+    public static String getPlayersDecision(String name) {
+        MessagePrinter.printMessage(
+                "" + '\n' +
+                "What is your decision, " + name + "?" + '\n' +
+                "Hit (H) or stand (S)?"
+        );
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+
 }
