b226f7e764d35f74857fbb751805ed93484c1a9f
matsarat <mat.saratowicz@gmail.com>
2020-11-17 23:41:25 +0100

    Main game class

--- src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
+++ src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
@@ -2,8 +2,9 @@ package com.example.java.maven.gameBlackJack;
 
 public class gameBlackJack {
     public static void main(String[] args) {
-       Card card = new Card();
-       card.deckBuilder();
+        Game game = new Game();
+
+
 
     }
 }
