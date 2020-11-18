904b0413bbffa90e1daac44eaca9c55660faa9a7
matsarat <mat.saratowicz@gmail.com>
2020-11-18 21:18:14 +0100

    Add condition of tie in game abd fix multiple asking for decision

--- src/main/java/com/example/java/maven/gameBlackJack/Game.java
+++ src/main/java/com/example/java/maven/gameBlackJack/Game.java
@@ -3,6 +3,7 @@ package com.example.java.maven.gameBlackJack;
 public class Game {
     Player player = new Player(UserInputProvider.getPlayersName());
     Croupier croupier = new Croupier();
+    boolean playerIsPlaying = true;
     private final Deck deck;
     private final static String INSTRUCTIONS =
             ('\n' + "INSTRUCTIONS:" + '\n' +
@@ -12,15 +13,17 @@ public class Game {
     private final static String INVALID_INPUT = ("Invalid input! Please stick to instructions!");
     private final String playerLost = ("No luck this time! You lost, " + player.name + "!");
     private final String playerWon = ("You won, " + player.name + ", you lucky guy!");
-    private final static String CROUPIER_TURN = ("Croupier's turn now.");
-    private final static String CROUPIER_LOST = ("Croupier lost.");
-    private final static String CROUPIER_WON = ("Croupier won.");
+    private final static String CROUPIER_TURN = ('\n' + "Croupier's turn now.");
+    private final static String CROUPIER_LOST = ('\n' + "Croupier lost.");
+    private final static String CROUPIER_WON = ('\n' + "Croupier won.");
+    private final static String TIE = ('\n' + "It's a tie!");
+
 
 
     public Game() {
         this.deck = new Deck();
         initiateGame();
-        while (playersDecision()) {
+        while (this.playerIsPlaying) {
             playersDecision();
         }
         gameFinishing();
@@ -33,7 +36,7 @@ public class Game {
         croupier.hand.add(deck.getCard());
 
         MessagePrinter.printPlayer(player);
-        MessagePrinter.printCroupier(croupier);
+        MessagePrinter.printCroupiersInitialHand(croupier);
 
         MessagePrinter.printMessage(INSTRUCTIONS);
     }
@@ -56,31 +59,41 @@ public class Game {
         return getPlayersDecision();
     }
 
-    public boolean playersDecision() {
-        if (player.points < 22) {
+    public void playersDecision() {
+        if (player.points < 21) {
             String userInput = getPlayersDecision();
             if (userInput.equalsIgnoreCase("H")) {
                 player.hand.add(deck.getCard());
                 MessagePrinter.printPlayer(player);
-                return true;
             }
         }
-        return false;
+        this.playerIsPlaying = false;
     }
 
     public void gameFinishing() {
         if (player.points == 21) {
             MessagePrinter.printMessage(playerWon);
+            MessagePrinter.printPlayer(player);
         } else if (player.points > 21) {
             MessagePrinter.printMessage(playerLost);
+            MessagePrinter.printPlayer(player);
         } else {
             croupiersPlay();
             if (croupier.points > 21) {
                 MessagePrinter.printMessage(CROUPIER_LOST);
-            } else if (croupier.points > player.points) {
+                MessagePrinter.printCroupier(croupier);
+            }
+            else if (croupier.points > player.points) {
+                MessagePrinter.printPlayerAndCroupier(player, croupier);
                 MessagePrinter.printMessage(CROUPIER_WON);
-            } else if (croupier.points < player.points) {
+            }
+            else if (croupier.points < player.points) {
+                MessagePrinter.printPlayerAndCroupier(player, croupier);
                 MessagePrinter.printMessage(playerWon);
+            }
+            else if (croupier.points == player.points) {
+                MessagePrinter.printPlayerAndCroupier(player, croupier);
+                MessagePrinter.printMessage(TIE);
             }
         }
     }
@@ -91,6 +104,6 @@ public class Game {
         while (croupier.getPoints() < 17) {
             croupier.hand.add(deck.getCard());
         }
-        MessagePrinter.printHand(croupier.hand);
+        MessagePrinter.printCroupier(croupier);
     }
 }
