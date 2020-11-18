979494048fd2c4ff25e9681f64fd64f5fa178fc3
matsarat <mat.saratowicz@gmail.com>
2020-11-18 19:19:17 +0100

    Delete unused method

--- src/main/java/com/example/java/maven/gameBlackJack/MessagePrinter.java
+++ src/main/java/com/example/java/maven/gameBlackJack/MessagePrinter.java
@@ -1,7 +1,5 @@
 package com.example.java.maven.gameBlackJack;
 
-import java.util.ArrayList;
-
 public class MessagePrinter {
 
     private MessagePrinter() {
@@ -13,10 +11,6 @@ public class MessagePrinter {
 
     public static void printError(String errorToPrint){
         System.err.println(errorToPrint);
-    }
-
-    public static void printHand(ArrayList<Card> hand) {
-        System.out.println(hand);
     }
 
     public static void printPlayer(Player player) {
