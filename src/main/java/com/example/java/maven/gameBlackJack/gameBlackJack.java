fdba0641db3fe5c86e596a392c80f46b0c9622cd
matsarat <mat.saratowicz@gmail.com>
2020-11-19 23:50:55 +0100

    Apply final game launching class, delete unused Croupier class

--- src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
+++ src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
@@ -2,7 +2,11 @@ package com.example.java.maven.gameBlackJack;
 
 public class gameBlackJack {
     public static void main(String[] args) {
-        Game game = new Game();
+        Player player = new Player(UserInputProvider.getPlayersName());
+        Player croupier = new Player("Croupier");
+        Deck deck = new Deck();
+
+        Game game = new Game(player, croupier, deck);
 
 
 
