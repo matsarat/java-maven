ffeecc87802258e4953cbbd2d3a6e82e780bc63b
matsarat <mat.saratowicz@gmail.com>
2020-11-19 12:25:09 +0100

    Delete redundant field, set fields to private final, create getter

--- src/main/java/com/example/java/maven/gameBlackJack/Player.java
+++ src/main/java/com/example/java/maven/gameBlackJack/Player.java
@@ -3,24 +3,26 @@ package com.example.java.maven.gameBlackJack;
 import java.util.ArrayList;
 
 public class Player {
-    String name;
-    ArrayList<Card> hand;
-    int points;
+    private final String name;
+    private final ArrayList<Card> hand;
 
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
-        this.points = getPoints();
     }
 
-    public void addCardToPlayersHand(Card card) {
-        hand.add(card);
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
     }
 
     public int getPoints() {
-        points = 0;
+        int points = 0;
         for (Card card : hand) {
-            points += card.rank.getValue();
+            points += card.getRank().getValue();
         }
         return points;
 
