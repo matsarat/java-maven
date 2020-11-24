89aabcac7b7ef0fc9f0f9ade219642070e478577
matsarat <mat.saratowicz@gmail.com>
2020-11-24 22:46:42 +0100

    Create GameTest class

---
+++ src/test/java/com/example/java/maven/gameBlackJack/GameTest.java
@@ -0,0 +1,11 @@
+package com.example.java.maven.gameBlackJack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+
+}
