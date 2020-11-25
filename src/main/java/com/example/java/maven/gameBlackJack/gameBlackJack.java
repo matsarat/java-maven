05dea0d9e2c3a7a2b35dd7c083e421b793b9d5ef
matsarat <mat.saratowicz@gmail.com>
2020-11-25 19:48:08 +0100

    Update changes from classes

--- src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
+++ src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
@@ -2,11 +2,13 @@ package com.example.java.maven.gameBlackJack;
 
 public class gameBlackJack {
     public static void main(String[] args) {
-        Player player = new Player(UserInputProvider.getPlayersName());
+        UserInputProvider userInputProvider = new UserInputProvider();
+        MessagePrinter messagePrinter = new MessagePrinter();
+        Player player = new Player(userInputProvider.getPlayersName());
         Player croupier = new Player("Croupier");
         Deck deck = new Deck();
 
-        Game game = new Game(player, croupier, deck);
+        Game game = new Game(player, croupier, deck, userInputProvider, messagePrinter);
 
 
 
