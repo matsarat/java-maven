7ace3abf39c8d36330190819d1d2ec5cfdc5602c
matsarat <mat.saratowicz@gmail.com>
2020-11-26 19:13:40 +0100

    Add tests for croupiersPlay and gameFinishing methods

--- src/test/java/com/example/java/maven/gameBlackJack/GameTest.java
+++ src/test/java/com/example/java/maven/gameBlackJack/GameTest.java
@@ -17,11 +17,13 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class GameTest {
+    private final static String TIE = ('\n' + "It's a tie!");
+    private final static String CROUPIER_TURN = ('\n' + "Croupier's turn now.");
     Game game;
     UserInputProvider userInputProvider = Mockito.mock(UserInputProvider.class);
-    MessagePrinter messagePrinter = new MessagePrinter();
+    MessagePrinter messagePrinter = Mockito.mock(MessagePrinter.class);
     Player player = Mockito.mock(Player.class);
-    Player croupier = Mockito.mock(Player.class);
+    Player croupier = new Player("Croupier");
     Deck deck = new Deck();
 
     @BeforeEach
@@ -106,8 +108,142 @@ class GameTest {
         verify(player, times(1)).getPoints();
         verify(userInputProvider, times(1))
                 .getPlayersDecision(eq(playerName));
+        verify(messagePrinter, times(1))
+                .printPlayer(player);
         assertThat(player.getHand().size()).isEqualTo(1);
         assertThat(game.playerIsPlaying).isTrue();
 
+    }
+
+    @Test
+    void shouldNotAddCardToCroupiersHand() {
+        List<Card> croupierHand = new ArrayList<>();
+
+//        given
+        given(croupier.getPoints()).willReturn(17);
+        given(croupier.getHand()).willReturn(croupierHand);
+
+//        when
+        game.croupiersPlay();
+
+//        then
+        assertThat(croupierHand).hasSize(0);
+        verify(messagePrinter, times(1)).printMessage(CROUPIER_TURN);
+    }
+
+    @Test
+    void shouldAddCardToCroupiersHand() {
+        Card card1 = new Card(Card.Suit.SPADE, Card.Rank.KING);
+        Card card2 = new Card(Card.Suit.SPADE, Card.Rank.SIX);
+        croupier.getHand().add(card1);
+        croupier.getHand().add(card2);
+
+//        given
+
+//        when
+        game.croupiersPlay();
+
+//        then
+        assertThat(croupier.getHand()).hasSize(3);
+        verify(messagePrinter, times(1))
+                .printMessage(CROUPIER_TURN);
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatPlayerInstantlyWon() {
+
+//        given
+        given(player.getPoints()).willReturn(21);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printMessage(game.playerWonMessage(player));
+        verify(messagePrinter, times(1))
+                .printPlayer(player);
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatCroupierInstantlyWon() {
+
+//        given
+        given(player.getPoints()).willReturn(22);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printMessage(game.playerWonMessage(croupier));
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatPlayerWonAfterCroupiersTurn() {
+
+//        given
+        given(croupier.getPoints()).willReturn(15);
+        given(croupier.getPoints()).willReturn(22);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printMessage(game.playerWonMessage(player));
+        verify(messagePrinter, times(1))
+                .printPlayer(croupier);
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatCroupierWonByPointsDifference() {
+
+//        given
+        given(player.getPoints()).willReturn(15);
+        given(croupier.getPoints()).willReturn(20);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printPlayerAndCroupier(player, croupier);
+        verify(messagePrinter, times(1))
+                .printMessage(game.playerWonMessage(croupier));
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatPlayerWonByPointsDifference() {
+
+//        given
+        given(player.getPoints()).willReturn(20);
+        given(croupier.getPoints()).willReturn(17);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printPlayerAndCroupier(player, croupier);
+        verify(messagePrinter, times(1))
+                .printMessage(game.playerWonMessage(player));
+    }
+
+    @Test
+    void shouldFinishGameAndPrintThatGameFinishedWithATie() {
+
+//        given
+        given(player.getPoints()).willReturn(20);
+        given(croupier.getPoints()).willReturn(20);
+
+//        when
+        game.gameFinishing();
+
+//        then
+        verify(messagePrinter, times(1))
+                .printPlayerAndCroupier(player, croupier);
+        verify(messagePrinter, times(1))
+                .printMessage(TIE);
     }
 }
