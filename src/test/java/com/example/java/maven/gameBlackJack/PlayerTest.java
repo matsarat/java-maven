90718cd698dbb64dc58ee6fafec5b945e152c4cc
matsarat <mat.saratowicz@gmail.com>
2020-11-26 21:20:36 +0100

    Delete nonsensical tests

--- src/test/java/com/example/java/maven/gameBlackJack/PlayerTest.java
+++ src/test/java/com/example/java/maven/gameBlackJack/PlayerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class PlayerTest {
     Player player = new Player("Januż");
-    Deck deck = new Deck();
-    private static final int NUMBER_OF_INITIAL_CARDS = 2;
 
 
     @Test
@@ -15,20 +13,4 @@ class PlayerTest {
         Assertions.assertEquals(0,player.getPoints());
     }
 
-
-    @Test
-    void playerShouldHaveMoreThanZeroPointsAfterDealingCards() {
-        for (int i = 0; i < NUMBER_OF_INITIAL_CARDS; i++) {
-            player.getHand().add(deck.getCard());
-        }
-        Assertions.assertNotEquals(0, player.getPoints());
-    }
-
-    @Test
-    void playerShouldHaveTwoCardsOnHand() {
-        for (int i = 0; i < NUMBER_OF_INITIAL_CARDS; i++) {
-            player.getHand().add(deck.getCard());
-        }
-        Assertions.assertEquals(2, player.getHand().size());
-    }
 }
