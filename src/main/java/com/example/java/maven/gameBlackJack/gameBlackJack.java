055039abed42b01d3c3ee141722a77183602e5b3
matsarat <mat.saratowicz@gmail.com>
2020-11-27 22:35:16 +0100

    Apply changes making the game able to handle more human players

--- src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
+++ src/main/java/com/example/java/maven/gameBlackJack/gameBlackJack.java
@@ -1,15 +1,23 @@
 package com.example.java.maven.gameBlackJack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class gameBlackJack {
     public static void main(String[] args) {
         UserInputProvider userInputProvider = new UserInputProvider();
         MessagePrinter messagePrinter = new MessagePrinter();
-        Player player = new Player(userInputProvider.getPlayersName());
+        int numberOfPlayers = userInputProvider.getNumberOfPlayers();
+        List<Player> humanPlayers = new ArrayList<>();
+        for (int i = 0; i < numberOfPlayers; i++) {
+            Player player = new Player(userInputProvider.getPlayersName());
+            humanPlayers.add(player);
+        }
         Player croupier = new Player("Croupier");
         Deck deck = new Deck();
 
-        Game game = new Game(player, croupier, deck, userInputProvider, messagePrinter);
+        Game game = new Game(humanPlayers, croupier, deck, userInputProvider, messagePrinter);
         GameService gameService = new GameService(game);
-        gameService.playGame();
+        gameService.playGame(humanPlayers);
     }
 }
